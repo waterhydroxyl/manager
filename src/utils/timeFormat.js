1caae4d26a59796712cda71d0f8cfb912ea6ebdc
waterhydroxyl <waterhydroxyl@gmail.com>
2022-04-24 19:10:33 +0800

    ci: (add) 打卡页面优化

--- src/utils/timeFormat.js
+++ src/utils/timeFormat.js
@@ -3,7 +3,7 @@ import utc from "dayjs/plugin/utc"
 
 dayjs.extend(utc)
 
-const TIME_FORMAT = "YYYY-MM-DD hh:mm:ss"
+const TIME_FORMAT = "YYYY-MM-DD"
 export const formatUtcTime = (utcTime, format = TIME_FORMAT) => {
   return dayjs.utc(utcTime).utcOffset(8).format(format)
 }
--- src/views/main/employeCT/workCheckIn/workCheckIn.vue
+++ src/views/main/employeCT/workCheckIn/workCheckIn.vue
@@ -1,32 +1,39 @@
 <template>
-  <div>
-    <el-card class="box-card" shadow="hover" style="width: 300px">
-      <!-- <div class="imageContainer" @click=""> -->
-      <el-image
-        style="width: 200px; height: 200px"
-        :src="isCheckIn ? imageUrlFalse : imageUrlTrue"
-        fit="cover"
-        @click="handleView"
-      ></el-image>
-      <!-- </div> -->
-    </el-card>
-    <el-card class="box-card" shadow="hover" style="width: 557px; height: 610px">
-      <el-calendar>
-        <!-- 这里使用的是 2.5 slot 语法，对于新项目请使用 2.6 slot 语法-->
-        <template v-slot:dateCell="{ data }">
-          <p :class="data.isSelected ? 'is-selected' : ''">
-            {{ data.day.split('-').slice(1).join('-') }}
-          </p>
-          <!-- <p>{{ typeof(data.day) }}</p> -->
-          <p v-if="calendarInfo[data.day]">{{ calendarInfo[data.day] }}</p>
-          <p v-else>未打卡</p>
-        </template>
-      </el-calendar>
-    </el-card>
+  <div class="container">
+    <div class="checkIn" v-if="isCheckIn">
+      <el-card class="box-card" shadow="hover" style="width: 300px">
+        <!-- <div class="imageContainer" @click=""> -->
+        <el-image
+          :src="isCheckIn ? imageUrlFalse : imageUrlTrue"
+          fit="cover"
+          @click="handleView"
+        ></el-image>
+        <!-- </div> -->
+      </el-card>
+    </div>
+    <div class="right" v-else>
+      <el-card class="box-card" shadow="hover" style="width: 557px; height: 610px">
+        <el-calendar v-model="selectDate">
+          <!-- 这里使用的是 2.5 slot 语法，对于新项目请使用 2.6 slot 语法-->
+          <template v-slot:dateCell="{ data }">
+            <p :class="data.isSelected ? 'is-selected' : ''">
+              {{ data.day.split('-').slice(1).join('-') }}
+            </p>
+            <!-- <p>{{ typeof(data.day) }}</p> -->
+            <p v-if="calendarInfo[data.day]">{{ calendarInfo[data.day] }}</p>
+            <p v-else>未打卡</p>
+          </template>
+        </el-calendar>
+      </el-card>
 
-    <el-card class="box-card" shadow="hover" style="width: 557px; height: 610px">
-      <span>详情</span>
-    </el-card>
+      <el-card class="box-card" shadow="hover" style="width: 557px; height: 610px">
+        <h2>打卡详情</h2>
+        <h3>打卡时间：{{ detailInfo.createTime }}</h3>
+        <h3>任务名称：{{detailInfo.taskName}}</h3>
+        <h3>备注信息：{{detailInfo.remark}}</h3>
+      </el-card>
+    </div>
+
     <PageModal
       ref="pageModalRef"
       :modalConfig="modalConfig"
@@ -42,6 +49,7 @@
 // import PageCard from '@/components/content/pageCard';
 import PageModal from '@/components/content/pageModal';
 /* 配置 */
+import { formatUtcTime } from '@/utils/timeFormat';
 import { myRequest } from '@/service';
 import { mapDataToOptions } from './utils';
 import { handleContentMixin } from '@/mixin/handleContentMixin';
@@ -50,11 +58,13 @@ export default {
   name: 'workCheckIn',
   data() {
     return {
+      selectDate: '',
       isCheckIn: true,
       imageUrlTrue: 'https://cloud.ilikestudy.cn//Typora25871649930952_.pic.jpg',
       imageUrlFalse: 'https://cloud.ilikestudy.cn//Typora25841649930874_.pic.jpg',
       loginUserId: 1,
       calendarInfo: {},
+      detailInfo: {},
     };
   },
   created() {
@@ -65,6 +75,17 @@ export default {
   },
   components: {
     PageModal,
+  },
+  watch: {
+    selectDate(newVal) {
+      console.log(formatUtcTime(newVal));
+      myRequest
+        .get(`/employee/getClockDetail/${this.loginUserId}/${formatUtcTime(newVal)}`)
+        .then((res) => {
+          this.detailInfo = res.data;
+          console.log(this.detailInfo);
+        });
+    },
   },
   mixins: [handleContentMixin],
   computed: {
@@ -83,7 +104,6 @@ export default {
     getTaskList() {
       myRequest.get(`/common/getTaskList`).then((res) => {
         modalConfig.formItems[0].options = mapDataToOptions(res.data);
-        console.log(modalConfig);
       });
     },
     getCheckInStatus() {
@@ -108,6 +128,13 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
 .el-card {
   display: inline-block;
   overflow: scroll;
